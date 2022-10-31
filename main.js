a7ca4482db8db8ef5c72b473dd460af7862334ce
sharifulislam141 <masudranadarkwolf@gmail.com>
2022-10-31 14:46:41 +0600

    dark theme update

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+// dark mood option start 
 document.getElementById('theme').addEventListener('click',function(){
     console.log("clicked")
-    document.body.classList.toggle('dark')
+    document.body.classList.toggle('dark');
+    if(document.body.classList.contains('dark')){
+        document.getElementById('mood').innerText = "Light Mood"
+
+    }
+    else{
+        document.getElementById('mood').innerText = "Dark Mood"
+    }
 })
+// dark mood option end 
